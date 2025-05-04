0c4d1e0da021a158fe23d64d6fafc26aff8fc9a5
klstxii <kiyoharusarmiento@gmail.com>
2025-05-04 21:55:20 +0800

    cleanup

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .products import products
 
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -15,7 +12,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
-# Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -49,39 +45,6 @@ def registerUser(request):
     except:
         message = {'detail':'User with this email already exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
- 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>',
-        '/api/users',
-        '/api/users/register',
-        '/api/users/login',
-        '/api/users/profile',
-
-        '/api/users/profile',
-        '/api/users/profile',
-        '/api/users/profile',
-        '/api/orders',
-        '/api/orders/add',
-
-    ]
-
-
-    return Response(routes)
-
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -95,18 +58,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
