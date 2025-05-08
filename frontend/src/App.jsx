679088716ae975025f7b961bdf63e001efc19c1d
klstxii <kiyoharusarmiento@gmail.com>
2025-05-08 16:16:38 +0800

    product edit screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import OrderScreen from "./screens/OrderScreen"
 import UserListScreen from "./screens/UserListScreen"
 import UserEditScreen from "./screens/UserEditScreen"
 import ProductListScreen from "./screens/ProductListScreen"
+import ProductEditScreen from "./screens/ProductEditScreen"
 
 function App() {
   return (
@@ -43,6 +44,8 @@ function App() {
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
 
             <Route path='/admin/productlist' element={<ProductListScreen />} />
+            <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+
           </Routes>
         
         </Container>
